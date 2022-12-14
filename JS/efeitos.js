9b6cd7c568b85157c510afb7369160e659b44648
tuliogs88 <86685611+tuliogs88@users.noreply.github.com>
2022-12-13 21:27:10 -0300

    Create efeitos.js

---
+++ JS/efeitos.js
@@ -0,0 +1,54 @@
+function Janela1(){
+    newWindow = window.open('','card',"width=640 height=480");
+    newWindow.document.write("<img src='Imagens/cilios.jpg'>");
+}
+
+function Janela2(){
+    newWindow = window.open('','card',"width=640 height=480");
+    newWindow.document.write("<img src='Imagens/designer_fio.jpg'>");
+}
+
+function Janela3(){
+    newWindow = window.open('','card',"width=640 height=480");
+    newWindow.document.write("<img src='Imagens/designer_henna2.jpg'>");
+}
+
+function Janela4(){
+    newWindow = window.open('','card',"with=640 height=480");
+    newWindow.document.write("<img src='Imagens/designer_henna_cilios.jpg'>");
+}
+
+function Janela5(){
+    newWindow = window.open('','card',"width=640 height=80");
+    newWindow.document.write("<img src='Imagens/designer_henna.jpg'>");
+}
+
+function Janela6(){
+    newWindow = window.open('','card',"width=640 hight=480");
+    newWindow.document.write("<img src='Imagens/designer.jpg'>");
+}
+
+function Janela7(){
+    newWindow = window.open('','card',"width640 height=480");
+    newWindow.document.write("<img src='Imagens/fio_a_fio.jpg'>");
+}
+
+function Janela8(){
+    newWindow = window.open('','card',"width=40 height=480");
+    newWindow.document.write("<img src='Imagens/micro.jpg'>");
+}
+
+function Janela9(){
+    newWindow = window.open('','card',"width=640 height=480");
+    newWindow.document.write("<img src='Imagens/foto 10.jpg'>");
+}
+
+function Janela10(){
+    newWindow = window.open('','card',"width=640 height=480");
+    newWindow.document.write("<img src='Imagens/foto 11.jpg'>");
+}
+
+function Janela11(){
+    newWindow = window.open('','card',"width=640 height=480");
+    newWindow.document.write("<img src='Imagens/foto 12.jpg'>");
+}
